273813dd0de3f5dcfc7c396f1674db2228ef3e72
Matvey <m5221211m@gmail.com>
2025-09-14 00:44:33 +0300

    init: initial commit

---
+++ src/main/java/org/example/freelancebackend/FreelanceBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.freelancebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelanceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreelanceBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/freelancebackend/FreelanceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.freelancebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelanceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
