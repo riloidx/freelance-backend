cb20a36f44580113cd98e6197faf3c34905c31c4
Matvey <m5221211m@gmail.com>
2025-10-05 18:04:11 +0300

    feat: add role mapping

---
+++ src/main/java/org/matvey/freelancebackend/roles/mapper/RoleMapper.java
@@ -0,0 +1,27 @@
+package org.matvey.freelancebackend.roles.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.matvey.freelancebackend.roles.dto.RoleResponseDto;
+import org.matvey.freelancebackend.roles.entity.Role;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface RoleMapper {
+    RoleResponseDto toDto(Role role);
+
+    List<RoleResponseDto> toDto(List<Role> roles);
+
+    default Set<String> mapRolesToStrings(Set<Role> roles) {
+        if (roles == null) {
+            return new HashSet<>();
+        }
+        return roles.stream()
+                .map(Role::getName)
+                .collect(Collectors.toSet());
+    }
+}
