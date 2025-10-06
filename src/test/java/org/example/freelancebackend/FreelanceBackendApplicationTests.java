374f14f90c24deac24348a42eb6516e5eeff6494
Matvey <m5221211m@gmail.com>
2025-10-06 08:58:39 +0300

    feat: create test application

--- src/test/java/org/example/freelancebackend/FreelanceBackendApplicationTests.java
+++ src/test/java/org/example/freelancebackend/FreelanceBackendApplicationTests.java
@@ -1,9 +1,12 @@
 package org.example.freelancebackend;
 
 import org.junit.jupiter.api.Test;
+import org.matvey.freelancebackend.FreelanceBackendApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = FreelanceBackendApplication.class)
+@ActiveProfiles("test")
 class FreelanceBackendApplicationTests {
 
     @Test
