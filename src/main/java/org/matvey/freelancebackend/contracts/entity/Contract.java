f92a45ea6968820e09a7a9cbf228d9cc32dbfcce
Matvey <m5221211m@gmail.com>
2025-10-19 23:59:16 +0300

    feat: add contract entity

---
+++ src/main/java/org/matvey/freelancebackend/contracts/entity/Contract.java
@@ -0,0 +1,42 @@
+package org.matvey.freelancebackend.contracts.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.matvey.freelancebackend.proposal.entity.Proposal;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "contracts")
+public class Contract {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "proposal_id")
+    private Proposal proposal;
+
+    @NotNull
+    @Column(name = "price", nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column(name = "contract_status", nullable = false, length = 64)
+    @Enumerated(EnumType.STRING)
+    private ContractStatus contractStatus =  ContractStatus.PENDING;
+
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+}
---
+++ src/main/java/org/matvey/freelancebackend/contracts/entity/ContractStatus.java
@@ -0,0 +1,7 @@
+package org.matvey.freelancebackend.contracts.entity;
+
+public enum ContractStatus {
+    PENDING,
+    APPROVED,
+    REJECTED,
+}
